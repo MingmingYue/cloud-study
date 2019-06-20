814146506111d4e90c71f190f33caf6b1c39030c
周明明 <zhoumm@yomob.com>
2019-06-20 15:36:11 +0800

    1. 基础信息

---
+++ zuul-gateway/src/main/java/com/ming/cloud/zuul/entity/User.java
@@ -0,0 +1,18 @@
+package com.ming.cloud.feignhystrix.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by 2019-06-20
+ */
+@Getter
+@Setter
+@Builder
+public class User {
+
+    private Long id;
+    private String username;
+    private Integer age;
+}
