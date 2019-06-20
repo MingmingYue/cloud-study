7ff6a63a18fb88b622bd2c53366adda050fe3be7
周明明 <zhoumm@yomob.com>
2019-06-20 16:44:11 +0800

    1. 基础信息

--- zipkin-server/src/main/java/com/ming/cloud/zipkin/ZipKinApplication.java
+++ zipkin-server/src/main/java/com/ming/cloud/zipkin/ZipKinApplication.java
@@ -3,7 +3,7 @@ package com.ming.cloud.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import zipkin.server.EnableZipkinServer;
+import zipkin2.server.internal.EnableZipkinServer;
 
 /**
  * Created by 2019-06-20
