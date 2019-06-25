6d7397a1ce941b331bf2c6458a214a0d7817a83c
周明明 <zhoumm@yomob.com>
2019-06-25 14:46:35 +0800

    1. 完善配置中心

--- user-server/src/main/java/com.ming.cloud.user/controller/UserController.java
+++ user-server/src/main/java/com.ming.cloud.user/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.ming.cloud.user.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
-    @Value("")
 
     @GetMapping("/index")
     public String index() {
