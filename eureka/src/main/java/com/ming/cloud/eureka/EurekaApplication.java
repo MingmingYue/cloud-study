2162db519d92ff4225fa177cba5f4bec4c941a81
zhoumingming <zhoumingming@1zhen.com>
2019-07-20 21:47:01 +0800

    1. 修改eureka

--- eureka/src/main/java/com/ming/cloud/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/ming/cloud/eureka/EurekaApplication.java
@@ -2,12 +2,14 @@ package com.ming.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * Created by 2019-06-19
  */
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EurekaApplication {
 
     public static void main(String[] args) {
