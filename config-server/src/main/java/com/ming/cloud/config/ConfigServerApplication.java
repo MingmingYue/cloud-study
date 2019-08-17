da12f249270f5206fda76218e1760c2295dac920
zhoumingming <zhoumingming@1zhen.com>
2019-08-17 18:46:46 +0800

    1. 修改配置文件

--- config-server/src/main/java/com/ming/cloud/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/ming/cloud/config/ConfigServerApplication.java
@@ -2,13 +2,15 @@ package com.ming.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by 2019-06-20
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
 @EnableConfigServer
 @EnableEurekaClient
 public class ConfigServerApplication {
--- eureka/src/main/java/com/ming/cloud/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/ming/cloud/eureka/EurekaApplication.java
@@ -3,13 +3,14 @@ package com.ming.cloud.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Created by 2019-06-19
  */
 @EnableEurekaServer
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
 public class EurekaApplication {
 
     public static void main(String[] args) {
