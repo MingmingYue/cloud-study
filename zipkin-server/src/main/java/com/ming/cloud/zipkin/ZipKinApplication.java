4146431296590b47cf42466ff6aff4656c325dcd
zhoumingming <zhoumingming@1zhen.com>
2019-08-21 11:58:38 +0800

    1. 去掉不必要的数据源

--- zipkin-server/src/main/java/com/ming/cloud/zipkin/ZipKinApplication.java
+++ zipkin-server/src/main/java/com/ming/cloud/zipkin/ZipKinApplication.java
@@ -2,18 +2,20 @@ package com.ming.cloud.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin2.server.internal.EnableZipkinServer;
 
 /**
  * Created by 2019-06-20
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
 @EnableZipkinServer
 @EnableEurekaClient
 public class ZipKinApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZipKinApplication.class,args);
+        SpringApplication.run(ZipKinApplication.class, args);
     }
 }
